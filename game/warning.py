962a58f69aabec2652b24995ba5480aa5ff66f7c
kielbassa <karol.narel@icloud.com>
2025-05-26 20:03:56 +0200

    made the warning sign larger

--- game/warning.py
+++ game/warning.py
@@ -3,18 +3,17 @@ import pygame as pg
 def create_warning_image():
     """
     Creates a warning image (exclamation mark).
-    Returns a pygame Surface containing a yellow exclamation mark on a red triangle.
     """
-    # Create a transparent surface
-    warning_img = pg.Surface((24, 24), pg.SRCALPHA)
-    
+
+    warning_img = pg.Surface((36, 36), pg.SRCALPHA)
+
     # Draw a red triangle
-    pg.draw.polygon(warning_img, (255, 0, 0), [(12, 0), (24, 22), (0, 22)], 0)
-    
+    pg.draw.polygon(warning_img, (255, 0, 0), [(18, 0), (36, 33), (0, 33)], 0)
+
     # Draw a yellow exclamation mark
     # The vertical line of the exclamation mark
-    pg.draw.rect(warning_img, (255, 255, 0), (10, 5, 4, 10), 0)
+    pg.draw.rect(warning_img, (255, 255, 0), (15, 8, 6, 15), 0)
     # The dot of the exclamation mark
-    pg.draw.rect(warning_img, (255, 255, 0), (10, 17, 4, 4), 0)
-    
+    pg.draw.rect(warning_img, (255, 255, 0), (15, 26, 6, 6), 0)
+
     return warning_img
